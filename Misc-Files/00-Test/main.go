1cbb1212e26bbebb6992806ce20bde049490bea0
Malav Naik <malavnaik12@gmail.com>
2024-06-03 21:57:41 -0500

    info about environment setup

---
+++ Misc-Files/00-Test/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/malavnaik12/Go-Sandbox/Udemy-Course/02-Dev-Env/Scope/furtherexplored"
+)
+
+func main() {
+	furtherexplored.Fascinating()
+}
---
+++ Udemy-Course/02-Dev-Env/Checksums/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// Check sum is a hash
+	// - What is a hash?
+	// Checksum is a way to check that the file that I downloaded is the one that Google intended to send me
+	// Usage: Just google "shasum"
+}
